195c5a883fe9520b6582d1c41e7388cc2b2b1126
yh-project <yoha6865@gmail.com>
2023-05-21 22:58:52 +0900

    Style/#13: 웹 사이트 배경색 및 네비게이션 바 디자인
    웹 사이트 배경색 설정
     - slate/200 으로 설정
    
    네비게이션 바 디자인
     - 상단에 fixed 로 디자인
     - 네비게이션 바 배경 sky/100 으로 설정

---
+++ src/components/Nav.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Nav(props) {
+  return (
+    <>
+      <div className="fixed top-0 left-0 right-0 flex justify-between w-screen border-b-[1px] h-20 bg-sky-100 border-slate-600">
+        <div className="flex items-center justify-between w-1/4 h-full ml-3">
+          <img
+            src=""
+            alt="Logo"
+            className="object-contain h-full px-2 py-3"
+          ></img>
+          <div className="text-base text-center text-slate-500">
+            여행하는 즐거움의 발견 유렌카!!
+          </div>
+        </div>
+        <div className="flex items-center w-1/4 h-full mr-5">
+          <ul className="flex justify-between w-full text-lg font-semibold list-none">
+            <li>고객센터</li>
+            <li>마이페이지</li>
+            <li>
+              <span className="text-amber-700">{props.name}</span> 고객님
+              안녕하세요!
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Nav;
