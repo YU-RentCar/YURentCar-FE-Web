e474f85b88f00dbc7e3f4a162fbd64d8d6a0bf78
seolbotdae <seolbotdae@gmail.com>
2023-05-22 19:57:59 +0900

    Style/#15: 날짜와 시간을 설정하는 컴포넌트의 UI 추가
    리스트를 볼 수 있는 버튼 아이콘을 추가
     - src/assets 에 추가
     - 기능은 추가하지 않음
    
    날짜와 시간을 설정하는 컴포넌트 제작
     - TimeSelectForm 컴포넌트
     - flex와 % 단위 넓이를 사용하여 동적으로 크기 조정 가능
     - 컴포넌트 사용시 width 속성을 지정하여 크기 조정 가능

---
+++ src/pages/homeSelectLocation/TimeSelectForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import listShowIcon from "../../assets/listShowIcon.png";
+
+function TimeSelectForm(props) {
+  const initSetting = "flex " + props.width;
+  let [isTimeClick, setIsTimeClick] = useState(false);
+
+  return (
+    <>
+      <form className={initSetting}>
+        <fieldset className="border-[7px] border-dashed border-slate-200 p-3 w-full">
+          <legend className="px-5 ml-12 text-2xl font-extrabold text-slate-600">
+            {props.legend}
+          </legend>
+          <div className="flex justify-around h-15 mt-[-12px]">
+            {/* 첫번째 버튼 */}
+            <div className="flex w-[50%] mr-5 justify-center items-center bg-sky-50 border-blue-500 border-[2px] rounded-full h-3/4">
+              <div className="flex justify-between w-4/5 ">
+                <div className="relative flex flex-row items-center justify-center w-2/3 text-2xl font-extrabold">
+                  {/* 날짜가 보이는 부분 */}
+                  2023-05-08
+                </div>
+
+                <button className="flex items-center">
+                  {/* 버튼을 통해 달력이 보이게 하는 부분 */}
+                  <img
+                    src={listShowIcon}
+                    alt="날짜 선택"
+                    className="object-contain h-3/4"
+                  ></img>
+                </button>
+              </div>
+            </div>
+
+            <button className="flex w-[40%] justify-center items-center bg-sky-50 border-blue-500 border-[2px] rounded-full h-3/4">
+              <div className="flex justify-between w-4/5 ">
+                <div className="flex items-center justify-center w-2/3 text-2xl font-extrabold">
+                  {/* 날짜가 보이는 부분 */}
+                  08:00
+                </div>
+
+                <button className="flex items-center">
+                  {/* 버튼을 통해 시간 리스트가 보이게 하는 부분 */}
+                  <img
+                    src={listShowIcon}
+                    alt="시간 선택"
+                    className="object-contain h-3/4"
+                  ></img>
+                </button>
+              </div>
+            </button>
+          </div>
+        </fieldset>
+      </form>
+    </>
+  );
+}
+
+export { TimeSelectForm };
