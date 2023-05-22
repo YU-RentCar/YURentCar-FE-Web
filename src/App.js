c6abd6aba7703ecef852bd29b31cf424ae8302d2
yh-project <yoha6865@gmail.com>
2023-05-22 10:53:36 +0900

    Style/#13: 웹 사이트 최상위 컨테이너 width height 수정
    width height 지정 방식 수정
     - 스크롤이 필요한 요소에서 제대로 적용되지 않는 문제 발생
     - screen 사이즈가 아닌 100% 만큼으로 수정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Nav from "./components/Nav";
 function App() {
   return (
     <>
-      <div className="flex-col items-center justify-between w-screen h-screen bg-slate-200">
+      <div className="flex-col items-center justify-between w-full h-full bg-slate-200">
         <Nav name="홍길동" />
       </div>
     </>
