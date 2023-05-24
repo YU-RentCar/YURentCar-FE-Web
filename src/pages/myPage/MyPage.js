eb951b8f49fbd33e0da21fb1af35c1bc78e5d39b
yh-project <yoha6865@gmail.com>
2023-05-24 13:05:11 +0900

    Style/#18: 마이페이지 사용자 기본 정보 UI 구현
    사용자 기본 정보 UI
     - 기본 정보에 표함된 요소에 따라 다른 UI
     - 닉네임 변경에 대한 UI 추가 구현

--- src/pages/myPage/MyPage.js
+++ src/pages/myPage/MyPage.js
@@ -1,8 +1,10 @@
 import React from "react";
-import ResvCardView from "./myPageResvCardView/MyPageResvCardView";
+import MyPageResvCardView from "./myPageResvCardView/MyPageResvCardView";
+import MyPageUserInfo from "./myPageUserInfo/MyPageUserInfo";
 import { useState } from "react";
 
 function MyPage() {
+  // 사용자의 예약 정보 객체
   let [resvInfo, setResvInfo] = useState({
     name: "홍길동",
     period: "2023-05-01 / 08:00 ~ 2023-05-31 / 22:00",
@@ -11,10 +13,20 @@ function MyPage() {
     number: "58부 7792",
     drivers: ["홍", "길", "동"],
   });
+  // 사용자 기본 정보 객체
+  let [userInfo, setUserInfo] = useState({
+    이름: "홍길동",
+    닉네임: "손씻은지도벌써백년",
+    전화번호: "010-0000-0000",
+    이메일: "gildong@gmail.com",
+  });
   return (
     <>
-      <div className="w-[calc(95%)] h-fit flex-col mx-auto pt-36">
-        <ResvCardView resvInfo={resvInfo} />
+      <div className="w-[1200px] h-fit mx-auto pt-36">
+        {/* 현재 예약 정보 카드뷰 */}
+        <MyPageResvCardView resvInfo={resvInfo} />
+        {/* 사용자 기본 정보 */}
+        <MyPageUserInfo userInfo={userInfo} />
       </div>
     </>
   );
---
+++ src/pages/myPage/myPageUserInfo/MyPageUserInfo.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { useState } from "react";
+import DefaultItem from "./internalComponents/DefaultItem";
+import ChangeNickname from "./internalComponents/ChangeNickname";
+
+/**
+ *
+ * @param {Object} userInfo 최상위 컴포넌트에서 받아온 사용자의 기본 정보 객체
+ * @returns
+ */
+function MyPageUserInfo(props) {
+  // 닉네임 변경하기 전과 변경을 하는 과정의 type
+  let [type, setType] = useState("default");
+  // type 에 따라 다른 컴포넌트를 호출
+  let ContentComponent = type === "default" ? DefaultItem : ChangeNickname;
+
+  let titles = Object.keys(props.userInfo);
+
+  return (
+    <>
+      <div className="w-full mt-20 h-fit">
+        {/* 기본 정보 타이틀 */}
+        <div className="flex items-center justify-center w-[15%] h-12 text-xl font-bold border-2 border-dashed rounded-md border-slate-400">
+          기본 정보
+        </div>
+        {/* 기본 정보들 */}
+        <div className="flex flex-col justify-around items-center w-full h-[60vh] bg-white border-4 rounded-md border-slate-400 px-28 py-5">
+          {titles.map((title) => {
+            return title !== "닉네임" ? (
+              // 닉네임이 아닌 경우는 기본 틀 (변경 버튼이 없음)
+              <DefaultItem
+                title={title}
+                content={props.userInfo[title]}
+                infoType={"hidden"}
+              />
+            ) : (
+              // 닉네임인 경우 (변경 버튼 추가)
+              // 버튼을 누름에 따라 바뀌는 상황 (변경 전 <-> 변경 과정)
+              <ContentComponent
+                title={title}
+                content={props.userInfo[title]}
+                infoType={"text-xl font-bold underline text-slate-400"}
+                setType={setType}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MyPageUserInfo;
