c0f9996784c37939629e0086d392c84b59c3e959
yh-project <yoha6865@gmail.com>
2023-06-02 16:55:26 +0900

    Refactor/#40: 소셜 로그인 하이퍼링크 추가
    소셜 로그인 버튼 별로 하이퍼링크 추가
     - 백엔드 서버에서 제공하는 링크 연결

--- src/pages/auth/Auth.js
+++ src/pages/auth/Auth.js
@@ -6,8 +6,24 @@ function Auth() {
   return (
     <div className="flex flex-col items-center justify-center w-[1200px] h-[100vh] overflow-x-hidden mx-auto">
       <img src={Logo} alt="logo" className="object-contain w-1/3 h-1/5"></img>
-      <img src={Naver} alt="naver" className="object-contain w-1/3 h-1/5"></img>
-      <img src={Kakao} alt="kakao" className="object-contain w-1/3 h-1/5"></img>
+      <div className="w-1/3 h-1/5">
+        <a href="http:/yurentcar.kro.kr:8080/oauth2/authorization/naver">
+          <img
+            src={Naver}
+            alt="naver"
+            className="object-contain w-full h-full"
+          ></img>
+        </a>
+      </div>
+      <div className="w-1/3 h-1/5">
+        <a href="http:/yurentcar.kro.kr:8080/oauth2/authorization/kakao">
+          <img
+            src={Kakao}
+            alt="kakao"
+            className="object-contain w-full h-full"
+          ></img>
+        </a>
+      </div>
     </div>
   );
 }
