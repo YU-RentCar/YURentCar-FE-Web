3dee178c6114f21fd5e3f290731402aa69e7078b
seolbotdae <seolbotdae@gmail.com>
2023-06-04 14:14:27 +0900

    Feat/#52: redux 에서 받아온 정보들로 선호 차량 검색을 초기화
    redux 에서 받아온 정보들로 선호 차량 검색을 초기화
     - 마이페이지에서 설정한 선호 차량 검색 조건을 받아와 input을 기본적으로 채움
     - 사용자는 검색 조건을 변경할 수 있음
    
    서버로 요청을 보낼 객체 생성
     - 서버로 요청을 보낼 때 필요한 객체를 미리 생성

--- src/pages/home/homeInquireCar/homeSearchPreferOption/internalComponents/CheckboxForm.js
+++ src/pages/home/homeInquireCar/homeSearchPreferOption/internalComponents/CheckboxForm.js
@@ -4,11 +4,14 @@ import { useState } from "react";
  * 체크박스 형태의 다중선택이 가능한 컴포넌트
  * @param {[string]} props.inputArray 부모에게 받아온 체크박스로 만들어줄 목록들
  * @param {setter} props.setCheckedList 부모에게 받아온 체크 여부를 확인하는 배열의 setter 내부는 [true, false] 같이 이루어진다.
+ * @param {[bool]} props.defaultCheckedList 부모에게 받아온 기본 체크 리스트
  */
 function CheckboxForm(props) {
-  let [innerCheckedList, setInnerCheckedList] = useState(
-    new Array(props.inputArray.length).fill(false)
-  );
+  console.log("test : " + props.defaultCheckedList);
+
+  let [innerCheckedList, setInnerCheckedList] = useState([
+    ...props.defaultCheckedList,
+  ]);
 
   return (
     <>
@@ -19,6 +22,7 @@ function CheckboxForm(props) {
               type="checkbox"
               key={idx}
               id={elm}
+              defaultChecked={innerCheckedList[idx]}
               onChange={(e) => {
                 let idx = props.inputArray.indexOf(e.target.id);
 
